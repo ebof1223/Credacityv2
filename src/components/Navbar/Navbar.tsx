da87242f67f35ccb9bf233035897eb3bbeda4727
Erwin Bofetiado <erwinbofetiado@Erwins-Air.attlocal.net>
2023-04-30 18:20:53 -0500

    nav spacing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,26 +7,28 @@ import { AppData } from "~/context/AppData";
 const Navbar = () => {
   const { reapply } = useContext(AppData);
   return (
-    <div className="navbar w-full bg-base-300">
-      <div className="flex-none lg:hidden">
-        <label htmlFor="my-drawer-3" className="btn-ghost btn-square btn">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-6 w-6 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </label>
-      </div>
-      <div className="btn-ghost btn mx-2 flex-1 px-2 text-xl normal-case lg:invisible ">
-        Churn.io
+    <div className="navbar flex w-full justify-between bg-base-300">
+      <div className="">
+        <div className="flex-none lg:hidden">
+          <label htmlFor="my-drawer-3" className="btn-ghost btn-square btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-6 w-6 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+        </div>
+        <div className="btn-ghost btn mx-2 px-2 text-xl normal-case lg:invisible lg:flex-1 ">
+          Churn.io
+        </div>
       </div>
       <div className=" flex-none ">
         <ul className="menu-horizontal">
